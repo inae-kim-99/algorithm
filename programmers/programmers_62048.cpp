27093c474e0c6ec5d66a5c0a3eee5e2c91a96977
Inae Kim <54172475+kia3415@users.noreply.github.com>
2021-03-28 14:46:39 +0900

    Update programmers_62048.cpp

--- programmers/programmers_62048.cpp
+++ programmers/programmers_62048.cpp
@@ -1,17 +1,15 @@
-// https://programmers.co.kr/learn/courses/30/lessons/62048
-
-int gcd(int a, int b){ // 최대공약수 return
-    if(b==0)
+int gcd(int a, int b) { // 최대공약수 return
+    if (b == 0)
         return a;
     else
-        return gcd(b, a%b);
+        return gcd(b, a % b);
 }
 
-long long solution(int w,int h) {
+long long solution(int w, int h) {
     long long answer = 1;
-    
-    long long num = gcd(w,h); // 최대공약수 구하기
-    
-    answer = (long long)w * (long long)h - ((w/num + h/num - 1) * num);
+
+    long long num = gcd(w, h); // 최대공약수 구하기
+
+    answer = (long long)w * (long long)h - (w + h - num);
     return answer;
 }
