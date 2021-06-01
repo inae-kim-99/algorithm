d7d7ca941b3b2623c928dc2914ff4b8151591aac
inae <kia3415@gmail.com>
2021-06-01 13:18:10 +0900

    릿코드 - Squares of a Sorted Array (3240)

---
+++ leetcode/leetcode_3240.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
+// Problem : Squares of a Sorted Array
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        
+        int[] list = new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++)
+            list[i] = nums[i]*nums[i];
+        
+        Arrays.sort(list);
+        
+        return list;
+    }
+}
