0c01e7bee830d5e476c9edae4608ba4b66ac70dc
inae <kia3415@gmail.com>
2021-06-01 10:59:42 +0900

    릿코드 - 첫번째 문제 풀이 (3238)

---
+++ leetcode/leetcode_3238.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
+// Problem : Max Consecutive Ones
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+        int count = 0, max = 0;
+        
+        for(int n : nums){
+            if(n == 1){
+                count++;
+                if(count > max)
+                    max = count;
+            } 
+            else{
+                count = 0;                
+            }
+        }
+        
+        return max;
+    }
+}
