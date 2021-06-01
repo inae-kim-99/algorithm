779095ad9370324631903c8d210a536c82c852b2
inae <kia3415@gmail.com>
2021-06-01 13:17:49 +0900

    릿코드 - Duplicate Zeros (3237)

---
+++ leetcode/leetcode_3237.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3237/
+// Problem : Find Numbers with Even Number of Digits
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        
+        int answer = 0;
+        
+        for(int n : nums){
+            
+            // Solve 1)
+            // if(Integer.toString(n).length() % 2 == 0)
+            //     answer++;
+            
+            // Solve 2)
+            int digits = 0;
+            while(n > 0){
+                n /= 10;
+                digits++;
+            }
+            
+            if(digits % 2 == 0)
+                answer++;
+        }
+        
+        return answer;
+    }
+}
