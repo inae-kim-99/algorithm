77201645b9a45b560a73efe1e73f6afb86578b7d
Kim In Ae <kia3415@gmail.com>
2022-02-28 16:41:26 +0900

    릿코드 - Remove Element (3247)

--- leetcode/leetcode_3247.py
+++ leetcode/leetcode_3247.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3247/
-# leetcode 3247 : deleting items from an array
+# leetcode 3247 : Remove Element
 
 class Solution:
     def removeElement(nums, val) -> int:
