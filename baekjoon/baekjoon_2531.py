51ca006be4a637951a28131d3968a051e01d5756
inae <kia3415@gmail.com>
2022-09-17 19:48:00 +0900

    백준 - 회전초밥 (2531), 두포인터

--- baekjoon/baekjoon_2531.py
+++ baekjoon/baekjoon_2531.py
@@ -5,32 +5,32 @@
 import sys
 from collections import defaultdict
 
-[N, d, k, c] = list(map(int, sys.stdin.readline().split()))
 
-belts = []
-eat, count = defaultdict(int), 0
-max_type = 0
+def solution():
+    N, d, k, c = map(int, sys.stdin.readline().split())
 
-for _ in range(N):
-    belts.append(int(sys.stdin.readline()))
+    nums = [int(sys.stdin.readline()) for _ in range(N)]
+    max_count = 0
 
-for idx in range(k): # condition 개수 만큼 먹는다.
-    if eat[belts[idx]] == 0:
-        count += 1
-    eat[belts[idx]] += 1
+    subset = defaultdict(int)
+    subset[c] += 1
+    count = 1
 
-for left in range(N+k): # 최댓값 계산 후, idx + 1부터 condition개를 연속으로 먹은 경우의 초밥 종류 수를 계산한다.
-    if eat[c] == 0:
-        max_type = max(count + 1, max_type)
-    else:
-        max_type = max(count, max_type)
+    for i in range(k): # 0 ~ k-1 초밥 먹기
+        if not subset[nums[i]]:
+            count += 1
+        subset[nums[i]] += 1     
     
-    eat[belts[left % N]] -= 1
-    if eat[belts[left % N]] == 0:
-        count -= 1
+    for i in range(N-1): # i번째 초밥빼고 i+k번째 초밥 먹었을때의 경우 계산하기
+        subset[nums[i]] -= 1
+        if not subset[nums[i]]:
+            count -= 1
+        if not subset[nums[(i+k) % N]]:
+            count += 1
+        subset[nums[(i+k) % N]] += 1
+        max_count = max(max_count, count)
     
-    eat[belts[(left+k) % N]] += 1
-    if eat[belts[(left+k) % N]] == 1:
-        count += 1
+    print(max_count)
 
-print(max_type)
+
+solution()
